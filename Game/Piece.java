a80a21cc402828c7066812a68348b19f64e80bc5
James Ekene <jideforekene42@gmail.com>
2021-08-28 21:51:02 -0400

    Added a rook piece

--- Game/Piece.java
+++ Game/Piece.java
@@ -1,5 +1,8 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Piece {
 	public Tile tile;
 	public Board board;
@@ -12,6 +15,22 @@ public abstract class Piece {
 	
 	public Tile position() {
 		return this.tile;
+	}
+	
+	public List<Tile> check(int a, int b){
+		int x = this.tile.row + a;
+		int y = this.tile.col + b;
+		List<Tile> lst = new ArrayList<>();
+		
+		
+		while (x>0 ^ x<9 ^ y>0 ^ y<9 ^ !this.board.tiles.get(x).get(y).has_piece) {
+			lst.add(this.board.tiles.get(x).get(y));
+			x += a;
+			y += b;
+			
+		}
+		
+		return lst;
 	}
 
 }
